440a4b798607dd7015c60475320e5cf12ce6bb34
RedH11 <44552711+RedH11@users.noreply.github.com>
2020-02-28 10:02:17 -0800

    Revert "Created the framework for testing new sizes of neural networks of different sizes"
    This reverts commit 361c1de4d42339d677a4c88c254dc812523e4620.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/NNVisionOnlyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/NNVisionOnlyTest.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.Tests;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.NNMode;
 import org.firstinspires.ftc.teamcode.Vision.NNVision;
 
 @Autonomous(name="Only Vision Test", group="Linear Opmode")
@@ -15,11 +14,7 @@ public class NNVisionOnlyTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Say", "Initializing...");
         telemetry.update();
-        // NNModes:
-        // initial - find starting locations and print them to telemetry as 0's and 1's
-        // display - show the identifications of the neural network ont he screen
-        // running - tbd ability to run the neural network to scout for stones on the go
-        vision = new NNVision(hardwareMap, telemetry, NNMode.initial);
+        vision = new NNVision(hardwareMap, telemetry);
         telemetry.addData("Say", "Done Initializing!");
 
         waitForStart();
