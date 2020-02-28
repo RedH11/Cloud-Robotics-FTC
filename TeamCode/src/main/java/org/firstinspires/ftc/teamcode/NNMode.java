a2e73b12978ed0f5671c7bc5f7eae4d48ef70a6c
RedH11 <44552711+RedH11@users.noreply.github.com>
2020-02-28 08:34:47 -0800

    Re-uploading to add NNMode as an enum to the branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NNMode.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public enum NNMode {
+    display, // Displaying its outputs for testing
+    initial, // Finding the initial placement of the blocks
+    running, // Finding the placements of the blocks while mid auto
+}
